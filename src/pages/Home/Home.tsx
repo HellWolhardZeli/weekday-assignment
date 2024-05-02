fb6e1420f4c91c9ad0850f781684a450d15c2fe5
Aditya Maharana <aditya.maharana@docsumo.com>
2024-05-02 22:19:12 +0530

    WIP - feat: home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,31 @@
-import SearchBox from "../../component/SearchBox/SearchBox";
+import { useEffect, useState } from "react";
+import CardList from "../../components/CardList/CardList";
 import styles from "./Home.module.scss";
+import { fetchData } from "../../services/api";
+import {
+  JdListResponse,
+  JobDescription,
+} from "../../services/interfaces/jobDescription";
 const Home = () => {
+  const [jdList, setJdList] = useState<JobDescription[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+
+  const fetchJdData = async () => {
+    const { jdList = [], totalCount = 0 } =
+      (await fetchData()) as JdListResponse;
+    setTotalCount(totalCount);
+    setJdList(jdList);
+  };
+  useEffect(() => {
+    fetchJdData();
+  });
+
   return (
     <div className={styles.root}>
-      <SearchBox />
+      <div className={styles.container}>Filters</div>
+      <div>
+        <CardList list={jdList} totalCount={totalCount} />
+      </div>
     </div>
   );
 };
